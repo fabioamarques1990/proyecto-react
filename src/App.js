614d3e6f23243f076bcb8cbd283c4c1c0ee50eb5
Fabio Marques <fabio.amarques1990@gmail.com>
2022-08-21 15:42:25 -0500

    Primer desafio.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let imagen = "../assets/background-image.jpg";
+  let titulo = "SUP San Luis Potosi";
+  let descripcion = "Ven a pasear con nosotros en el lugar mas precioso que vas a ver en tu vida";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='row'>
+        <div class="col-md-4 offset-md-2">
+          <img src={imagen} alt={titulo} className="img-fluid"/>
+        </div>
+        <div class="col-md-4">
+          <h1>{titulo}</h1>
+          <p>{descripcion}</p>
+        </div>
+      </div>
     </div>
   );
 }
