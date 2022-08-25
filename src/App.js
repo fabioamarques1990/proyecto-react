c522860f6439453105edd6cb63b03db4665a8ddd
Fabio Marques <fabio.amarques1990@gmail.com>
2022-08-24 20:33:33 -0500

    Algo.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
+import 'core-js/actual';
+import Header from './components/Header';
 
 function App() {
-  let imagen = "../assets/background-image.jpg";
-  let titulo = "SUP San Luis Potosi";
-  let descripcion = "Ven a pasear con nosotros en el lugar mas precioso que vas a ver en tu vida";
 
   return (
-    <div className="container">
-      <div className='row'>
-        <div class="col-md-4 offset-md-2">
-          <img src={imagen} alt={titulo} className="img-fluid"/>
-        </div>
-        <div class="col-md-4">
-          <h1>{titulo}</h1>
-          <p>{descripcion}</p>
-        </div>
-      </div>
+    <div>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <h1>React JS</h1>
+            <hr />
+        </div>
+    )
+}
+
+export default Header;
