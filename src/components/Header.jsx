5bfb260527d354b60a11417c96010365997ae274
Fabio Marques <fabio.amarques1990@gmail.com>
2022-08-29 20:58:16 -0500

    Segundo desafio completado.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import logo from "../assets/sup-porto.jpg";
 
 const Header = () => {
     return (
         <div>
-            <h1>React JS</h1>
-            <hr />
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <img src={logo} alt="SUP PORTO" width="90"/>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                            <a className="nav-link" href="#">Paquetes</a>
+                            <a className="nav-link" href="#">Contacto</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
         </div>
     )
 }
