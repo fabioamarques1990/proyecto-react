e5735a6bb57f576eb0b8a90e3f12b52a66d230bc
Fabio Marques <fabio.amarques1990@gmail.com>
2022-09-05 21:21:30 -0500

    Agregue un itemCount, de manera a que se pueda agregar paquetes, y con las reglas correctas, sin que pueda bajar de cero y no rebasando el stock existente.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'core-js/actual';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import ItemCount from './components/ItemCount';
 
 function App() {
   const lista = ["Paquete 1", "Paquete 2", "Paquete 3", "Paquete 4"];
@@ -16,6 +17,9 @@ function App() {
       <ul>
         {items}
       </ul>
+
+      <ItemCount stock={8} initial={1} onAdd={0} />
+
     </div>
   );
 }
---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useState } from "react";
+
+const ItemCount = ({ stock, initial, onAdd }) => {
+    const [cantidad, setCantidad] = useState(initial);
+    const [itemStock, setItemStock] = useState(stock);
+    const [itemAdd, setItemAdd] = useState(onAdd);
+
+    const restar = (valor) => {
+        if (valor > 0) {
+            setCantidad(valor);
+        }
+    }
+
+    const sumar = (valor) => {
+        if (valor <= itemStock) {
+            setCantidad(valor);
+        }
+    }
+
+    const agregarPaquetes = () => {
+        if (cantidad <= itemStock) {
+            setItemStock(itemStock - cantidad);
+            setItemAdd(itemAdd + cantidad);
+        }
+    }
+
+    return (
+        <div className="container py-5">
+            <div className="row">
+                <div className="col-md-2">
+                    <h3 className="text-center">Paquete 2</h3>
+                    <div className="input-group">
+                        <input type="button" className="btn btn-secondary" value="-" onClick={() => { restar(cantidad - 1) }} />
+                        <input type="text" className="form-control" value={cantidad} onChange={() => { }} />
+                        <input type="button" className="btn btn-secondary" value="+" onClick={() => { sumar(cantidad + 1) }} />
+                    </div>
+                    <div className="d-grid gap-2 pt-3">
+                        <input type="button" className="btn btn-secondary" value="Agregar" onClick={() => {agregarPaquetes()}} />
+                    </div>
+                    <br />
+                    <h5>Paquetes seleccionados: {itemAdd}</h5>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ItemCount;
